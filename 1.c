da1a9bb4cf0b45a3e5fe6011405688fdcd6de92e
U-lisicheng-PC\lisicheng <lisicheng@lisicheng-PC.(none)>
2010-11-20 15:04:38 +0800

    modified 1.c and 8.c

--- 1.c
+++ 1.c
@@ -1,19 +1,64 @@
-abrupt - Adjective: Sudden and unexpected.
-accessory - Noun: A thing that can be added to something else in order to make it more useful. Someone who gives assistance to the perpetrator of a crime.
-adjunct - Noun: A thing added to something else as a supplementary rather than an essential part.
-acclaim - Verb: Pralse enthusiastically and publicly.
-accord - Verb: Give or grant someone. Noun: An offical agreement or treaty.
-acute - Adjective: Of a bad, difficult, or unwelcome situation or phenomenon. Present or experienced to a severe or intense degree.
-adhesive - Adjective: Able to stick fast to a surface or object; sticky. Noun: A substance used for sticking objects or materials together; glue.
-
-adjacent - Adjective: Next to or adjoining something else.
-adorn - Verb: Make more beautiful or attractive.
-beautify - (same as above).
-advent - Noun: The arrival or a notable person, thing or event. The coming or second coming of Christ.
-adverse - Adjective: Preventing success or development; harmful unfavorable.
-adversity - Noun: Difficulties, misfortune.
-aesthetic - Noun: A set or principles underlying and guiding the work of a particular artist or artistic movement. Adjective, Concerned with beauty or the appreciation of beauty.
-afflict - Verb: Cause pain or suffering to; affect or trouble.
-
+abrupt
+{
+Adjective: Sudden and unexpected.
+}
+accessory
+{
+Noun: A thing that can be added to something else in order to make it more useful. Someone who gives assistance to the perpetrator of a crime.
+}
+adjunct
+{
+Noun: A thing added to something else as a supplementary rather than an essential part.
+}
+acclaim
+{
+Verb: Praise enthusiastically and publicly.
+}
 accord
+{
+Verb: Give or grant someone. 
+Noun: An offical agreement or treaty.
+}
+acute
+{
+Adjective: Of a bad, difficult, or unwelcome situation or phenomenon. Present or experienced to a severe or intense degree.
+}
+adhesive
+{
+Adjective: Able to stick fast to a surface or object; sticky. 
+Noun: A substance used for sticking objects or materials together; glue.
+}
+adjacent
+{
+Adjective: Next to or adjoining something else.
+}
+adorn
+{
+Verb: Make more beautiful or attractive.
+}
+beautify
+{
+(same as above).
+}
+advent
+{
+Noun: The arrival or a notable person, thing or event. The coming or second coming of Christ.
+}
+adverse
+{
+Adjective: Preventing success or development; harmful unfavorable.
+}
+adversity
+{
+Noun: Difficulties, misfortune.
+}
+aesthetic
+{
+Noun: A set or principles underlying and guiding the work of a particular artist or artistic movement. 
+Adjective: Concerned with beauty or the appreciation of beauty.
+}
+afflict
+{
+Verb: Cause pain or suffering to; affect or trouble.
+}
 
