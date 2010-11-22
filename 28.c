1e3f06b4ea6565ba610b04bd506a451e50dd1e5b
lisicheng <lisicheng2008@gmail.com>
2010-11-22 14:21:12 +0800

    Fulfilled 28.c
    Signed-off-by: lisicheng <lisicheng2008@gmail.com>

--- 28.c
+++ 28.c
@@ -71,38 +71,220 @@ Noun:The use of humor, irony, exaggeration, or ridicule to expose and criticize 
 }
 satiric
 saturate
+{
+Adjective:Saturated with moisture.
+Noun:A saturated fat.
+Verb:Cause (something) to become thoroughly soaked with liquid so that no more can be absorbed.
+}
 Saturn
+{
+Noun:An ancient god, regarded as a god of agriculture.
+}
 sauce
+{
+Noun:Thick liquid served with food, usually savory dishes, to add moistness and flavor.
+Verb:Provide a sauce for (something); season with a sauce.
+}
 scar
+{
+Noun:A mark left on the skin or within body tissue where a wound, burn, or sore has not healed completely and fibrous connective tissue has developed.
+Verb:Mark with a scar or scars.
+}
 scarce
+{
+Adjective:(esp. of food, money, or some other resource)Insufficient for the demand.
+    Occurring in small numbers or quantities; rare.
+Adverb:Scarcely.
+}
 scarf
+{
+Verb:Join the ends of(two pieces of timber or metal)by beveling or notching them so that they fit over or into each other.
+Noun:A length or square of fabric worn around the neck or head.
+Verb:Eat or drink(something) hungrily or enthusiastically.
+Noun:A joint connecting two pieces of timber or metal in which the ends are veveled  or notched so that they fit over or into each other.
+}
 scatter
+{
+Noun:A small, dispersed amount of something.
+    The degree to which repeated measurements or observations of a quantity differ.
+Verb:Throw in various random directions.
+}
 scavenger
+{
+Noun:An animal that feeds on carrion, dead plant material, or refuse.
+    A person who searches for and collects discarded items.
+    A person employed to clean the streets.
+}
 scent
+{
+Verb:Impart a pleasant scent to. Discern by the sense of smell.
+Noun:A distinctive smell, esp. one that is pleasant. Pleasant-smelling liquid worn on the skin; perfume.
+}
 scholar
+{
+Noun:A specialist in a particular branch of study, esp. the humanities; a distinguished academic.
+    A person who is highly educated or has an aptitude for study.
+}
 scorch
+{
+Noun:The burning or charring of the surface of something.
+Verb:Burn the surface of(something)with flame or heat.
+}
 scout
+{
+Noun:A soldier or other person sent out ahead of a main force so as to gather information about the enemy's position, strength, or movements.
+Verb:Make a search for someone or something in various places.
+}
 scrape
+{
+Noun:An act or sound of scraping. An injury or mark caused by scraping.
+Verb:Push or pull a hard or sharp implement across(a surface or object)so as to remove dirt or other matter.
+}
 scratch
+{
+Adjective:Assembled or made from whatever is available, and so unlikely to be of the highest quality.
+    (of a sports competitor or event)With no handicap given.
+Verb:Score or mark the surface of(something) with a sharp or pointed object.
+    Make a long, narrow superficial wound in the skin of.
+Noun:A mark or wound made by scratching.
+}
 scrub
+{
+Verb:Rub (someone or something)hard so as to clean them, typicallly with a brush and water
+    Remove dirt by rubbing hard.
+Noun:An act of scrubbing something or someone.
+Noun:Land covered with such vegetation.
+    Vegetation consisting mainly of brushwood or stunted forest growth.
+}
 scruffy
+{
+Adjective:Shabby and untidy or dirty. Dressed in scruffy jeans and a baggy T-shirt.
+}
 scuba
+aqualung
+{
+Noun:A portable breathing apparatus for divers, consisting of cylinders of compressed air strapped on the diver's back, feeding air automatically through a mask or mouthpiece.
+Verb:Swim underwater using such an apparatus.
+}
 sculpt
+{
+Verb:Create or represent (something)by carving, casting, or other shaping techniques.
+}
 sculpture
+{
+Noun:The art of making two- or three-dimensional representative or abstract forms, esp. by carving stone or wood or by casting metal or plaster.
+    A work of such a kind.
+Verb:Make or represent(a form)by carving, casting, or other shaping techniques.
+    Form, shape, or mark as if by sculpture, esp. with strong, smooth curves.
+}
 scurry
+{
+Verb:(of a person or small animal)Move hurriedly with short quick steps.
+Noun:A situation of hurried and confused movement.
+}
 seal
+{
+Verb:Fasten or close securely.
+    Prevent something from escaping by closing a container or opening.
+Noun:A device or substance that is used to join two things together so as to prevent them from coming apart or to prevent anything from passing between them.
+Noun:A fish-eating aquatic mammal with a streamlined body and feet developed as flippers, returning to land to breed or rest.
+}
 secrete
+{
+Verb:(of a cell, gland, or organ)Produce and discharge (a substance).
+Verb:Conceal; hide.
+}
 sedentary
+{
+Adjective:(of a person)Tending to spend much time seated; somewhat inactive.
+    (of work or a way of life)Characterized by much sitting and little physical exercise.
+}
 sediment
+{
+Verb:Settle as sediment. (of a liquid)Deposit a sediment. Deposit(something)as a sediment.
+Noun:Matter that settles to the bottom of a liquid; dregs.
+}
 seep
+{
+Verb:(of liquid)Flow or leak slowly through porous material or small holes.
+Noun:A place where petroleum or water oozes slowly out of the ground.
+}
 segregate
+{
+Noun:An allete that has undergone segregation.
+    A species within an aggregate.
+Verb:Set apart from the rest of from each other; isolate or divide.
+    Separate or divide(people, activities, or institutions)along racial, sexual, or religious lines.
+}
 seismic
+{
+Adjective:Of or relating to earthquakes or other vibrations of the earth and its crust.
+    Relating to or denoting geological surveying methods involving vibrations produced artificially by explosions.
+}
 seismograph
+{
+Noun:An instrument that measures and records details of earthquakes, such as force and duration.
+}
 seismology
+{
+Noun:The branch of science concerned with earthquakes and related phenomena.
+}
 semester
+{
+Noun:A half-year term in a school or college, typically lasting fifteen to eighteen weeks.
+}
 seminar
+{
+Noun:A conference or other meeting for discussion or training.
+    A class at a college or university in which a topic is discussed by a teacher and a small group of students.
+}
 senate
+{
+Noun:Any of various legislative or governing bodies, in particular.
+    The smaller upper assembly in the US Congress, most US states, France, and other countries.
+}
 sensory
+{
+Adjective:Of or relating to sensation or the physical senses; transmitted or percieved by the senses.
+}
 sentimental
+{
+Adjective:Of or prompted by feelings of tenderness, sadness, or nostalgia.
+    (of a person)Excessively prone to feelings of tenderness, sadness, or nostalgia.
+}
 sequoia
+{
+Noun:A redwood tree, esp. the California redwood.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
