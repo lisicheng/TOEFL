2fb4f6110345c0c135e7ab32e56d00384c415507
Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>
2010-11-25 13:31:49 +0800

    add meanings for 29.c
    Signed-off-by: Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>

--- 29.c
+++ 29.c
@@ -1,42 +1,216 @@
 sew
+{
+Verb:Join, fasten, or repair (something)by making stitches with a needle and thread or a sewing maching.
+    Attach (something)to something else by sewing.
+}
 sewage
+{
+Noun:Waste water and excrement conveyed in sewers.
+}
 shade
+{
+Verb:Screen from direct light. Cover, moderate, or exclude the light of.
+Noun:Comparative darkness and coolness caused by shelter from direct sunlight.
+}
 shallow
+{
+Verb:(of the sea, a lake, or a river)Become less deep over time or in a particular place.
+Adjective:Of little depth. Situated at no great depth.
+}
 shatter
+{
+Verb:Break or cause to break suddenly and violently into pieces.
+    Damage or destroy (something abstract).
+}
 shear
+{
+Noun:A strain in the structure of a substance produced by pressure, when its layers are laterally shifted in relation to each other.
+Verb:Cut the wool off(a sheep or other animal).
+    Cut off(something such as hair, wool, or grass), with scissors or shears.
+}
 shed
+{
+Noun:A simple roofed structure, typically made of wood or metal, used as a storage space, a shelter for animals, or a workshop.
+Verb:Park (a vehicle)in a depot.
+Verb:(of a tree or other plant) Allow(leaves or fruit)to fall to the ground.
+}
 sheer
+{
+Noun:sheers; A sudden deviation from a course, esp. by a boat.
+Noun:The upward slope of a ship's lines toward the bow and stern.
+Adverb:Perpendicularly. Completely; right.
+Verb:(typically of a boat or ship)Swerve or change course quickly.
+Noun:A very fine or diaphanous fabric or article.
+Adjective:Nothing other than; unmitigated(used for emphasis).
+}
+perpendicular
 shield
+{
+Noun:A broad piece of metal or another suitable material, held by straps or a handle attached on one side, used as a protection against blows or missiles.
+Verb:Protect(someone or something)from a danger, risk, or unpleasant experience.
+}
 shovel
+{
+Verb:Move (coal, earth, snow, or similar material)with a shovel.
+    Remove snow from (an area)with a shovel.
+Noun:A tool with a broad flat blade and typically upturned sides, used for moving coal, earth, snow or other material.
+}
 shower
+{
+Noun:A brief and usually light fall of rain, hail, sleet, or snow.
+    A mass of small things falling or moving at the same time.
+Verb:(of a mass of small things)Fall or be thrown in a shower.
+}
 shrimp prawn lobster
+{
+Verb:Fish for shrimp.
+Noun:A small free-swimming crustacean with an elongated body, typically marine and frequently harvested for food.
+}
 shrink
+{
+Verb:Become or make smaller in size or amount; contract or cause to contract.
+    (of clothes or material)Become smaller as a result of being immersed in water.
+Noun:A clinical psychologist, psychiatrist, or psychocherapist.
+}
 shroud
+{
+Verb:Wrap or dress (a body)in a shroud for burial.
+    Cover or envelop so as to conceal from view.
+Noun:A length of cloth or an envelping garment in which a dead person is wrapped for burial.
+}
 shrub
+{
+Noun:A drink made of sweetened fruit juice and liquor, typically rum or brandy.
+Noun:A woody plant that is smaller than a tree and has several main stems arising at or near the ground.
+}
 sieve
+{
+Verb:Put (a food substance or other material)through a sieve. Examine in detail.
+Noun:A utensil consisting of a wire or plastic mesh held in a frame, used for straining solids from liquids, for separating coarser from finer particles, or for reducing soft solids to a pulp.
+}
 signify
+{
+Verb:Be an indication of. Be a symbol of; have as meaning. (of a person)Indicate or declare(a feeling or intention).
+}
 silica
+{
+Noun:A hard, unreactive, colorless compound that occurs as the mineral quartz and as a principal constituent of sandstone and other rocks.
+}
 silt
+{
+Verb:Become filled or blocked with silt.
+Noun:Find sand, clay, or other material carried by running water and deposited as a sediment, esp. in a channel or harbor.
+}
 simmer
+{
+Verb:(of water or food)Stay just below the boiling point while being heated.
+    Keep (something)at such a point when cooking or heating it.
+Noun:A state or temperature just below the boiling point.
+}
 simultaneous
+{
+Adjective:Occurring, operating, or done at the same time.
+}
+spontaneous
 sinuous
+{
+Adjective:Having many curves and turns.
+    Lithe and supple.
+}
 skeletal
+{
+Adjective:Of, relating to, or functioning as a skeleton.
+    Very thin; emaciated. Existing only in outline or as a framework of something.
+}
 skeptical
+{
+Adjective:Not easily convinced; having doubts or reservations.
+    Relating to the theory that certain knowledge is impossible.
+}
 sketch
+{
+Verb:Make a rough drawing of. Give a brief account or general outline of.
+Noun:A rough or unfinished drawing or painting, often made to assist in making a more finished picture.
+}
 skull
+{
+Noun:A framework of bone or cartilage enclosing the brain of a vertebrate; the skeleton of a person's or animal's head.
+Verb:Hit(someone)on the head.
+}
 sled
+{
+Noun:A vehicle on runners for traveling over snow or ice, either pushed or pulled, drawn by horses, dogs, or a motor vehicle, or allowed to slide downhill.
+Verb:Ride on a sled.
+}
 sledding
 slender
+{
+Adjective:(of a person or part of the body)Gracefully thin.
+    (esp. of a rod or stem)Of small girth or breadth.
+    (of something abstract)Barely sufficient in amount or basis.
+}
 slide
+{
+Noun:A structure with a smooth sloping surface for children to slide down.
+Verb:Move along a smooth surface while maintaining continuous contact with it.
+}
 slip
+{
+Noun:A small piece of paper, typically a form for writing on or one giving printed information.
+    A long, narrow strip of a thin material such as wood.
+Noun:A fall to lower level or standard.
+Verb:(of a person or animal)Slide unintentionally for a short distance, typically losing one's balance or footing.
+}
 slog
+{
+Noun:A spell of difficult, tiring work or traveling.
+Verb:Work hard over a period of time. Walk or move with difficulty or effort.
+}
 sloth
+{
+Noun:Reluctance to work or make an effort; laziness.
+}
+reluctance
+{
+Noun:Unwillingness or disinclination to do something.
+}
 slumber
+{
+Verb:Sleep.
+Noun:A sleep.
+}
 smother
+{
+Noun:A mass of something that stifles or obscures.
+Verb:Kill(someone)by covering their nose and mouth so that they suffocate.
+}
 snap
+{
+Verb:Break or cause to break suddenly and completely, typically with a sharp cracking sound.
+Adjective:Done or taken on the spur of the moment, unexpectedly, or without notice.
+Noun:A sudden, sharp cracking sound or movement.
+}
 snowflake
+{
+Noun:A flake of snow, esp. a feathery ice crystal, typically displaying delicate sixfold symmetry.
+}
 sodium
+{
+Noun:The chemical element of atomic number 11, a soft silver-white reactive metal of the alkali metal group.
+}
 solder
+{
+Noun:A low-melting alloy, esp. one based on lead and tin or(for higher temperatures)on brass or silver, used for joining less fusible metals.
+Verb:Join with solder.
+}
 solicit
+{
+Verb:Ask for or try to obtain (something)from someone.
+    Ask(someone)for something.
+}
 solitary
+{
+Adjective:Done or existing alone. (of a place)Secluded or isolated.
+Noun:A recluse or hermit.
+}
 
