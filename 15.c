e28d4ce81c6a0a701be30f585ff3e60a1f117d71
Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>
2010-11-26 13:39:55 +0800

    15.c and 16.c
    Signed-off-by: Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>

--- 15.c
+++ 15.c
@@ -1,36 +1,147 @@
 hardy
+{
+Adjective:Robust; capable of enduring difficult conditions.
+    (of a plant)Able to survive outside during winter.
+}
 harness
-harsh - Adjective:Unpleasantly rough or jarring to the senses. Cruel or severe.
-haul - Verb: (of a person)Pull or drag witheffort or force. Noun: The amount of something gained or acquired.
-haunt - Verb: Be persistently in the mind of(someone). Noun: A place frequented by a specified person or group of people.
-heed - Verb: Pay attention to, take notice of. Noun: Careful attention.
-herald - Verb: Be a sign that (something) is about to happen, acclaim. Noun: An official messenger bringing news.
-herb - Any plant with leaves, seeds, or lowers used for flavoring, food, medicine, or perfume.
-herd - Verb: Move in a particular direction, keep or look after. Noun: A large group of animals, esp. hoofed mammals, that live, feed, or migrate together.
-hierarchy - Noun: A system or organization in which people or groups are ranked one above the other according to status or authority.
-hieratic - monk
-hind - Noun: A female deer, esp. a red deer. Adjective: (esp. of a bodily part) Situated at the back; posterior.
-hinder - Verb: Create difficulties for (someone or something). Adjective: (esp. of a bodily part)Rear; hind
+{
+Verb:Put a harness on (a horse or draft animal).
+    Attach a draft animal to (something) by a harness.
+Noun:A set of straps and fittings by which a horse or other draft animal is fastened to a cart, plow.
+}
+harsh
+{
+Adjective:Unpleasantly rough or jarring to the senses. Cruel or severe.
+}
+haul
+{
+Verb: (of a person)Pull or drag with effort or force. 
+Noun: The amount of something gained or acquired.
+}
+haunt
+{
+Verb: Be persistently in the mind of(someone). 
+Noun: A place frequented by a specified person or group of people.
+}
+heed
+{
+Verb: Pay attention to, take notice of. 
+Noun: Careful attention.
+}
+herald
+{
+Verb: Be a sign that (something) is about to happen, acclaim. 
+Noun: An official messenger bringing news.
+}
+herb
+{
+Noun:Any plant with leaves, seeds, or lowers used for flavoring, food, medicine, or perfume.
+}
+herd
+{
+Verb: Move in a particular direction, keep or look after. 
+Noun: A large group of animals, esp. hoofed mammals, that live, feed, or migrate together.
+}
+hierarchy
+{
+Noun: A system or organization in which people or groups are ranked one above the other according to status or authority.
+}
+hieratic
+{
+monk
+}
+hind
+{
+Noun: A female deer, esp. a red deer. 
+Adjective: (esp. of a bodily part) Situated at the back; posterior.
+}
+hinder
+{
+Verb: Create difficulties for (someone or something). 
+Adjective: (esp. of a bodily part)Rear; hind
+}
 hinterland
-hitherto - Adverb: Until now or until the point in time under discussion.
-hive - Verb: Place(bees) in a hive. Noun: A beehive, a place in which people are busily occupied.
+hitherto
+{
+Adverb: Until now or until the point in time under discussion.
+}
+hive
+{
+Verb: Place(bees) in a hive. 
+Noun: A beehive, a place in which people are busily occupied.
+}
 hockey
 hollow
-homestead - Noun: a house, esp. a farm house, and out buildings.
-hominid - Noun: A primate of a family(Hominidae) that includes humans and their fossil ancestors.
-homogeneous - Adjective: Of the same kind; alike.
+homestead
+{
+Noun: a house, esp. a farm house, and out buildings.
+}
+hominid
+{
+Noun: A primate of a family(Hominidae) that includes humans and their fossil ancestors.
+}
+homogeneous
+{
+Adjective: Of the same kind; alike.
+}
 chaos
-horn - Noun: A hard permanent outgrowth often curved and painted. found in pairs on the heads of cattle.
+horn
+{
+Noun: A hard permanent outgrowth often curved and painted. found in pairs on the heads of cattle.
+}
 hostile
-hover - Verb: Remain in one place in the air.
-huddle - Verb: Crowd together; nestle closely. Noun: a crowd of confused mass of people.
-humid - Adjective: Marked by a relatively high level of water vapor in the atmosphere. moist, damp.
-hurl - Verb: throw a object with great force.
-husk - Verb: Remove the husk or husks from. Noun: the dry outer covering of some fruits or seeds.
-hydrothermal - Adjective: Of relating to, or denoting the action of heated water in the earth's crust.
-hygiene - Nound: Conditions or practices conducive to maintaining health and preventing disease. esp. through cleaniness. personal hygiene.
-hypothesize - Verb: Put something forward as a hypothesis.
-ideology - Noun: A system of ideas and ideals, esp. one that forms the basis of economic or political theory and policy.
-idle - Verb: (of a person) Spending time doing nothing, be idle.
-ignorant - Adjective: Lacking knowledge information or awareness about something in particular.
+{
+Adjective:Unfriendly, antagonistic.
+    Of or belonging to a military enemy.
+}
+hover
+{
+Verb: Remain in one place in the air.
+}
+huddle
+{
+Verb: Crowd together; nestle closely. 
+Noun: a crowd of confused mass of people.
+}
+humid
+{
+Adjective: Marked by a relatively high level of water vapor in the atmosphere. moist, damp.
+}
+hurl
+{
+Verb: throw a object with great force.
+}
+husk
+{
+Verb: Remove the husk or husks from. 
+Noun: the dry outer covering of some fruits or seeds.
+}
+hydrothermal
+{
+Adjective: Of relating to, or denoting the action of heated water in the earth's crust.
+}
+hygiene
+{
+Nound: Conditions or practices conducive to maintaining health and preventing disease. esp. through cleaniness. personal hygiene.
+}
+hypothesize
+{
+Verb: Put something forward as a hypothesis.
+}
+ideology
+{
+Noun: A system of ideas and ideals, esp. one that forms the basis of economic or political theory and policy.
+}
+idle
+{
+Verb: (of a person) Spending time doing nothing, be idle.
+}
+ignorant
+{
+Adjective: Lacking knowledge information or awareness about something in particular.
+}
 
+unfamiliar
+{
+hurl
+}
--- 16.c
+++ 16.c
@@ -1,67 +1,249 @@
 imagist
-imbibe - Verb: drink. Absorb or assimilate(ideas or knowledge).
-immense - Adjective: Extremely large or great, esp. in scale or degree.
-immunity - Noun: The ability of an organism to resist a particular infection or toxin by the aciton of specific antibodies ro sensitized white blood cells.
-immutable - Adjective: Unchanging over time or unable to be changed.
-impact - Noun: The action of one object coming forcibly into contact with another. Verb: Come into forceble contact with another object. Come into forcible contac with.
-impair - Verb: Weaken or damage something(esp. a human faculty or function).
-impart - Verb: Make(information)known;communicate.
-impend - Verb: Be about to happen. (of something bad)Loom.
+imbibe
+{
+Verb: drink. Absorb or assimilate(ideas or knowledge).
+}
+assimilate
+{
+Verb:Take in(information, ideas, or culture)and understand fully.
+}
+immense
+{
+Adjective: Extremely large or great, esp. in scale or degree.
+}
+immunity
+{
+Noun: The ability of an organism to resist a particular infection or toxin by the aciton of specific antibodies or sensitized white blood cells.
+}
+immutable
+{
+Adjective: Unchanging over time or unable to be changed.
+}
+impact
+{
+Noun: The action of one object coming forcibly into contact with another. 
+Verb: Come into forceble contact with another object. Come into forcible contac with.
+}
+impair
+{
+Verb: Weaken or damage something(esp. a human faculty or function).
+}
+impart
+{
+Verb: Make(information)known;communicate.
+}
+impend
+{
+Verb: Be about to happen. (of something bad)Loom.
+}
+imperative
+{
+Noun: An essential or urgent thing. 
+Adjective:Of vital importance; curcial.
+}
+impermeable
+{
+Adjective: Not allowing fluid to pass through. Not liable to be affected by pain or distress; insusceptible or imperturbable.
+}
+impersonal
+{
+Adjective: Not influenced by. showing. or involving personal feelings.
+}
+impetus
+{
+Noun: The force or energy with which a body moves. The force that makes something happen or happen more quickly.
+}
+implement
+{
+Verb: Put(a decision, plan, agreement, etc.)into effect. 
+Noun:A tool, utensil, or other piece of equipment, esp. as used for a particular purpose.
+}
+utensil
+{
+Noun:A implement, container, or other article, esp. for household use.
+}
+implication
+{
+Noun: The conclusion that can be drawn from something, although it is not explicitly stated. A likely consequence of something.
+}
+implicit
+{
+Adjective: Implied though not plainly expressed. Essentially or very colsely connected with; always to be found in.
+}
+imply
+{
+Verb: Strongly suggest the truth or existence of(something not expressly stated).
+}
+impose
+{
+Verb: Force(something unwelcome or unfamiliar)to be accepted or put in place. Forcibly put(a restriction) in place.
+}
+imprint
+{
+Noun: A mark made by pressing something on to a softer substance so that its outline is reproduced. 
+Verb:Press or stamp(a mark or outline)on a surface or body.
+}
+improvise
+{
+Verb: Create and perform(music, drama, or verse)spontaneously or without preparation.
+}
+inanimate
+{
+Adjective: Not alive, esp. not in the manner of animals and humans. Showing no sign of life; lifeless.
+}
+inanity
+{
+hollow;
+}
+inaugurate
+{
+Verb: Begin or introduce(a system, policy, or period). Admit(someone)formally to public office.
+}
+incense
+{
+Verb: Perfume with incense or a similar fragrance. 
+Verb:Make(someone)very angry. 
+Noun:A gum, spice, or other sustance that is burned for the sweet smell it produces.
+}
+incentive
+{
+Noun: A thing that motivates or encourages one to do something. A payment or concession to stimulate greater output or investment.
+}
+inception
+{
+Noun: The establishment or starting point of an institution or activity.
+}
+incessant
+{
+Adjective: (of something regarded as unpleasant)Continuing without pause or interruption.
+}
+incinerate
+{
+Verb: Destroy(something, esp. waste material)by burning.
+}
+inclination
+{
+Noun: A person's natural tendency or urge to act or feel in a particular way; a disposition or propensity.
+}
+inclined
+{
+Noun: An inclined surface or slope, esp. on a road, path, or railway. 
+Verb: Feel willing or favorably diposed toward(an action, belief, or attitude).
+}
+inconspicuous
+{
+Adjective: Not clearly visible or attracting attention; not conspicuous.
+}
+incorporate
+{
+Verb: Put or take in(something)as part of a whole; include. Combine(ingredients)into one substance. 
+Adjective:Having a bodily form; enbodied.
+}
+incubate
+{
+Verb:(of a bird)Sit on(eggs)in order to keep them warm and bring them to hatching. Have an infectious disease developing inside one before symptoms appear.
+}
+incursion
+{
+Noun:An invasion or attack, esp. a sudden or brief one. eg.incusions into enemy territory.
+}
+indifference
+{
+Noun: Lack of interest, concern, or sympathy. Unimportance.
+}
+indigestion
+{
+Noun: Pain or discomfort in the stomach associated with difficulty in digesting food.
+}
+dispensable
+{
+Adjective: About to be replaced or done without.
+}
+indispensable
+{
+Adjective: Absolutely necessary. dispensable;
+}
+induce
+{
+Verb: Succeed in persuading or influencing(someone)to do something.
+}
+inert
+{
+Adjective: Lacking the ability or strength to move. Lacking vigor.
+}
+vigor
+{
+Noun: Physical strength and good health. Effort, energy, and enthusiasm.
+}
+inevitably
+{
+Adverb: As is certain to happen; unavoidably. As one would expect; predictably.
+}
+infancy
+{
+Noun: The state or period of early childhood or babyhood. The early stage in the development or growth of something.
+}
+infant
+{
+Noun:A very young child or baby. Denoting something in an early stage of its development.
+}
+infectious
+{
+Adjective:(of a disease of disease-causing organism)Likely to be transmitted to people, organisms, etc., through the environment.
+}
+inferior
+{
+Noun: A person lower than another in rank, status, or ability. 
+Adjective: Lower in rank, status, or quality. Of low standard or quality.
+}
+inflammation
+{
+Noun: A localized physical condition in which part of the body becomes reddened, swollen, hot, and often painful, esp. as a reaction to injury or infection.
+}
+influx
+{
+Noun: An arrival or entry of large numbers of people or things. An inflow of water into a river, lake, or the sea.
+}
+influenza
+{
+Noun:A highly contagious viral infection of the respiratory pasages causing fever, severe aching, and catarrh, and often occurring in epidemics.
+}
+infrared
+{
+Adjective: (of electromagnetic radiation)Having a wavelength just greater than that of the red end of the visible light spectrum but less than that of microwaves. Infrared radiation has a wavelength from about 800nm to 1nm, and is emitted particularly by heated objects. 
+Noun: The infrared region of the spectrum; infrared radiation.
+}
+infrastructure
+{
+Noun: The basic physical and organizational structures and facilities(e.g., building, roads, and power supplies)needed for the operation of a society or enterprise.
+}
+ingenious
+{
+Adjective:(of a person)Clever, original, and inventive.(of a maching or idea)Cleverly and originally devised and well suited to its purpose.
+}
+ingredient
+{
+Noun: Any of the foods or substances that are combined to make a particular dish. A component part or element of something.
+}
+inhabit
+{
+Verb: (of a person, animal, or group)Live in or occupy(a place or environment).
+}
+inherent
+{
+Adjective: Existing in something as a permanent, essential, or characteristic attribute. Vested in(someone)as a right or priviledge.
+}
+inherit
+{
+Verb: Receive(money, property, or a title)as an heir at the death of the previous holder. Derive(a quality, characteristic, or perdisposition)genetically from one's parents or ancestors.
+}
+inhibit
+{
+Verb: Hinder, restrain, or prevent(an action or process). Prevent or prohibit(someone) from doing something.
+}
 
-imperative - Noun: An essential or urgent thing. Adjective:Of vital  importance; curcial.
-impermeable - Adjective: Not allowing fluid to pass through. Not liable to be affected by pain or distress; insusceptible or imperturbable.
-impersonal - Adjective: Not influenced by. showing. or involving personal feelings.
-impetus - Noun: The force or energy with which a body moves. The force that makes something happen or happen more quickly.
-implement - Verb: Put(a decision, plan, agreement, etc.)into effect. Noun:A tool, utensil, or other piece of equipment, esp. as used for a particular purpose.
-implication - Noun: The conclusion that can be drawn from something, although it is not explicitly stated. A likely consequence of something.
-implicit - Adjective: Implied though not plainly expressed. Essentially or very colsely connected with; always to be found in.
-imply - Verb: Strongly suggest the truth or existence of(something not expressly stated).
-impose - Verb: Force(something unwelcome or unfamiliar)to be accepted or put in place. Forcibly put(a restriction) in place.
-imprint - Noun: A mark made by pressing something on to a softer substance so that its outline is reproduced. Verb:Press or stamp(a mark or outline)on a surface or body.
-
-improvise - Verb: Create and perform(music, drama, or verse)spontaneously or without preparation.
-
-inanimate - Adjective: Not alive, esp. not in the manner of animals and humans. Showing no sign of life; lifeless.
-inanity - hollow;
-inaugurate - Verb: Begin or introduce(a system, policy, or period). Admit(someone)formally to public office.
-incense - Verb: Perfume with incense or a similar fragrance. Verb:Make(someone)very angry. Noun:A gum, spice, or other sustance that is burned for the sweet smell it produces.
-incentive - Noun: A thing that motivates or encourages one to do something. A payment or concession to stimulate greater output or investment.
-inception - Noun: The establishment or starting point of an institution or activity.
-incessant - Adjective: (of something regarded as unpleasant)Continuing without pause or interruption.
-incinerate - Verb: Destroy(something, esp. waste material)by burning.
-inclination - Noun: A person's natural tendency or urge to act or feel in a particular way; a disposition or propensity.
-inclined - Noun: An inclined surface or slope, esp. on a road, path, or railway. Verb: Feel willing or favorably diposed toward(an action, belief, or attitude).
-inconspicuous - Adjective: Not clearly visible or attracting attention; not conspicuous.
-incorporate - Verb: Put or take in(something)as part of a whole; include. Combine(ingredients)into one substance. Adjective:Having a bodily form; enbodied.
-incubate - Verb:(of a bird)Sit on(eggs)in order to keep them warm and bring them to hatching. Have an infectious disease developing inside one before symptoms appear.
-incursion - Noun:An invasion or attack, esp. a sudden or brief one. eg.incusions into enemy territory.
-indifference - Noun: Lack of interest, concern, or sympathy. Unimportance.
-indigestion - Noun: Pain or discomfort in the stomach associated with difficulty in digesting food.
-dispensable - Adjective: About to be replaced or done without.
-indispensable - Adjective: Absolutely necessary. dispensable;
-
-induce - Verb: Succeed in persuading or influencing(someone)to do something.
-inert - Adjective: Lacking the ability or strength to move. Lacking vigor.
-vigor - Noun: Physical strength and good health. Effort, energy, and enthusiasm.
-
-inevitably - Adverb: As is certain to happen; unavoidably. As one would expect; predictably.
-infancy - Noun: The state or period of early childhood or babyhood. The early stage in the development or growth of something.
-infant - Noun:A very young child or baby. Denoting something in an early stage of its development.
-infectious - Adjective:(of a disease of disease-causing organism)Likely to be transmitted to people, organisms, etc., through the environment.
-inferior - Noun: A person lower than another in rank, status, or ability. Adjective: Lower in rank, status, or quality. Of low standard or quality.
-inflammation - Noun: A localized physical condition in which part of the body becomes reddened, swollen, hot, and often painful, esp. as a reaction to injury or infection.
-influx - Noun: An arrival or entry of large numbers of people or things. An inflow of water into a river, lake, or the sea.
-influenza - Noun:A highly contagious viral infection of the respiratory pasages causing fever, severe aching, and catarrh, and often occurring in epidemics.
-infrared - Adjective: (of electromagnetic radiation)Having a wavelength just greater than that of the red end of the visible light spectrum but less than that of microwaves. Infrared radiation has a wavelength from about 800nm to 1nm, and is emitted particularly by heated objects. Noun: The infrared region of the spectrum; infrared radiation.
-infrastructure - Noun: The basic physical and organizational structures and facilities(e.g., building, roads, and power supplies)needed for the operation of a society or enterprise.
-ingenious - Adjective:(of a person)Clever, original, and inventive.(of a maching or idea)Cleverly and originally devised and well suited to its purpose.
-ingredient - Noun: Any of the foods or substances that are combined to make a particular dish. A component part or element of something.
-inhabit - Verb: (of a person, animal, or group)Live in or occupy(a place or environment).
-inherent - Adjective: Existing in something as a permanent, essential, or characteristic attribute. Vested in(someone)as a right or priviledge.
-inherit - Verb: Receive(money, property, or a title)as an heir at the death of the previous holder. Derive(a quality, characteristic, or perdisposition)genetically from one's parents or ancestors.
-inhibit - Verb: Hinder, restrain, or prevent(an action or process). Prevent or prohibit(someone) from doing something.
-
-//added
+unfamiliar
+{
 impart
 imperative
 impermeable
@@ -80,3 +262,5 @@ incense
 incessant
 induce
 ingredient
+}
+
