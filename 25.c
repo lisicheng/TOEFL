d04b2c5bf3dd8b481e3a1b5abd9bd7c278938f92
Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>
2010-12-03 12:02:35 +0800

    25 and 26 formatted
    Signed-off-by: Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>

--- 25.c
+++ 25.c
@@ -1,40 +1,170 @@
-prodigious - Adjective:Remarkably or impressively great in extent, size, or degree. Unnatural or abnormal.
-proficient - Noun:A person who is proficient. Adjective:Competent or skilled in doing or using something. I was proficient at my job.
-profound - Adjective:(of a state, quality, or emotion)Very great or intense.(of a subject or thought)Demanding deep study or thought. Noun:The vast depth of the ocean or of the mind.
-prohibitive - Adjective:(of a price or charge)Excessively high; difficult or impossible to pay.(esp. of a law or rule)Forbidding or restricting something.
-prolific - Adjective:(of a plant, animal, or person)Producing much fruit or foliage or many offspring.(of an artist, author, or composer)Producing many works.
-prolonged - Verb:Extend the duration of. Extend in spatial length.
-
-prominent - Adjective:Important; famouns.
-prompt - Adjective:Done without delay;immediate. Verb:(of an event or fact)Cause or bring about(an action or feeling). Noun:An act of assisting or encouraging a hesitating speaker.
-pronounced - Adjective:Very noticeable or marked;conspicuous.
-proofread - Verb:Read(printer's proofs or other written or printed material)and mark any errors.
-proponent - Noun:A person who advocates a theory, proposal, or project.
-propose - Verb:Put forward(an idea or plan)for consideration or discussion by others. Nominate(someone)for an elected office or as a member of a society.
-prose - Noun:Written or spoken language in its ordinary form, without metrical structure. Plain or dull writing, discourse, or expression. Verb:Talk tediously. Compose or convert into prose.
-
-prospect - Noun:The possibility that something will happen. Verb:To search an area for gold, minerals, oil, etc.
-prototype - Noun:A first or preliminary model of something, esp. a machine, from which other forms are developed or copied. Verb:Make a prototype of(a product).
-prudent - Adjective:Acting with or showing care and thought for the future.
-psychoanalysis - Noun:A system of psychological theory and therapy that aims to treat mental disorders by investigating the interaction of conscious and unconscious elements in the mind and bringing repressed fears and confilicts into the onscious mind by techniques such as dream interpretation and free association.
-puddle - Noun:A small pool or liquid esp. of rainwater on the ground. Verb:Wet or cover(a surface)with water, esp. rainwater.
+prodigious
+{
+Adjective:Remarkably or impressively great in extent, size, or degree. Unnatural or abnormal.
+}
+proficient
+{
+Noun:A person who is proficient. 
+Adjective:Competent or skilled in doing or using something. I was proficient at my job.
+}
+profound
+{
+Adjective:(of a state, quality, or emotion)Very great or intense.(of a subject or thought)Demanding deep study or thought. 
+Noun:The vast depth of the ocean or of the mind.
+}
+prohibitive
+{
+Adjective:(of a price or charge)Excessively high; difficult or impossible to pay.(esp. of a law or rule)Forbidding or restricting something.
+}
+prolific
+{
+Adjective:(of a plant, animal, or person)Producing much fruit or foliage or many offspring.(of an artist, author, or composer)Producing many works.
+}
+prolonged
+{
+Verb:Extend the duration of. Extend in spatial length.
+}
+prominent
+{
+Adjective:Important; famouns.
+}
+prompt
+{
+Adjective:Done without delay;immediate. 
+Verb:(of an event or fact)Cause or bring about(an action or feeling). 
+Noun:An act of assisting or encouraging a hesitating speaker.
+}
+pronounced
+{
+Adjective:Very noticeable or marked;conspicuous.
+}
+proofread
+{
+Verb:Read(printer's proofs or other written or printed material)and mark any errors.
+}
+proponent
+{
+Noun:A person who advocates a theory, proposal, or project.
+}
+propose
+{
+Verb:Put forward(an idea or plan)for consideration or discussion by others. Nominate(someone)for an elected office or as a member of a society.
+}
+prose
+{
+Noun:Written or spoken language in its ordinary form, without metrical structure. Plain or dull writing, discourse, or expression. 
+Verb:Talk tediously. Compose or convert into prose.
+}
+prospect
+{
+Noun:The possibility that something will happen. 
+Verb:To search an area for gold, minerals, oil, etc.
+}
+prototype
+{
+Noun:A first or preliminary model of something, esp. a machine, from which other forms are developed or copied. 
+Verb:Make a prototype of(a product).
+}
+prudent
+{
+Adjective:Acting with or showing care and thought for the future.
+}
+psychoanalysis
+{
+Noun:A system of psychological theory and therapy that aims to treat mental disorders by investigating the interaction of conscious and unconscious elements in the mind and bringing repressed fears and confilicts into the onscious mind by techniques such as dream interpretation and free association.
+}
+puddle
+{
+Noun:A small pool or liquid esp. of rainwater on the ground. 
+Verb:Wet or cover(a surface)with water, esp. rainwater.
+}
 pueblo
-pulp - Noun:A soft, wet, shapeless mass of material. The soft fleshy part of a fruit. Verb:Crush into a soft, shapeless mass. Withdraw(a publication)from the market and recycle the paper.
-punctual - Adjective:Happening or doing something at the agreed or proper time;on time. Denoting or relating to an action that takes place at a particular point in time.
-pupil - Noun:A student in school. Noun:The dark circular opening in the center of the iris of the eye, varying in size to regulate the amount of light reaching the retina.
-purple - Verb:Become or make purple in color. Noun:A color intermediate between red and blue.
-pursuit - Noun:The action of following or pursuing someone or something.
-pyramid - Noun:A monumental structure with a square of trangular base and sloping sides that meet in a point at the top, esp. one built of stone as a royal tomb in ancient Egypt. Verb:Heap or stack in the shape of a pyramid.
-
-quaint - Adjective:Attractively unusual or old-fashioned.
-quarry - Verb:Extract(stone or other materials)from a quarry. A diamond-shaped pane of glass as used in lattice windows. An animal pursued by a hunter, hound, predatory mammals, or bird of prey.
-quasar - Noun:A massive and extremely remote celestial object, emitting exceptionally large amounts of energy, and typically having a starlike image in a telescope. It has been suggested that quasars contain massive black holes and may represent a stage in the evolution of some galaxies.
-quilt - Noun:A warm bed covering made of padding enclosed between layers of fabric and kept in place by lines of stitching, typically applied in a decorative design.
-quiz - Noun:quizzes. A test of knowledge, esp.a brief, informal test given to students. Noun:A practical joke or hoax; a piece of banter or ridicule.
-quota - Noun:A limited or fixed number or amount of people or things, in particular.A fixed share of something that a person or group is entitled to receive from a total.
+pulp
+{
+Noun:A soft, wet, shapeless mass of material. The soft fleshy part of a fruit. 
+Verb:Crush into a soft, shapeless mass. Withdraw(a publication)from the market and recycle the paper.
+}
+punctual
+{
+Adjective:Happening or doing something at the agreed or proper time;on time. Denoting or relating to an action that takes place at a particular point in time.
+}
+pupil
+{
+Noun:A student in school. 
+Noun:The dark circular opening in the center of the iris of the eye, varying in size to regulate the amount of light reaching the retina.
+}
+purple
+{
+Verb:Become or make purple in color. 
+Noun:A color intermediate between red and blue.
+}
+pursuit
+{
+Noun:The action of following or pursuing someone or something.
+}
+pyramid
+{
+Noun:A monumental structure with a square of trangular base and sloping sides that meet in a point at the top, esp. one built of stone as a royal tomb in ancient Egypt. 
+Verb:Heap or stack in the shape of a pyramid.
+}
+quaint
+{
+Adjective:Attractively unusual or old-fashioned.
+}
+quarry
+{
+Verb:Extract(stone or other materials)from a quarry. A diamond-shaped pane of glass as used in lattice windows. An animal pursued by a hunter, hound, predatory mammals, or bird of prey.
+}
+quasar
+{
+Noun:A massive and extremely remote celestial object, emitting exceptionally large amounts of energy, and typically having a starlike image in a telescope. It has been suggested that quasars contain massive black holes and may represent a stage in the evolution of some galaxies.
+}
+quilt
+{
+Noun:A warm bed covering made of padding enclosed between layers of fabric and kept in place by lines of stitching, typically applied in a decorative design.
+}
+quiz
+{
+Noun:quizzes. A test of knowledge, esp.a brief, informal test given to students. 
+Noun:A practical joke or hoax; a piece of banter or ridicule.
+}
+quota
+{
+Noun:A limited or fixed number or amount of people or things, in particular.A fixed share of something that a person or group is entitled to receive from a total.
+}
 quote
-racket - Noun:A type of bat with a round or oval frame strung with catgut, nylon, etc., used esp. in tennis, badminton, and squash.
-radical - Noun:A person who advocates thorough or complete political or social reform; a member of a political party or part of a party pusuing such aims. Adjective:(esp. of change or action)Relating to or affecting the fundamental nature of something;far-reaching or thorough.
-rage - Noun:Violent, uncontrollable anger. The violent action of a natural agency. Verb:Feel or express violent uncontrollable anger.
+racket
+{
+Noun:A type of bat with a round or oval frame strung with catgut, nylon, etc., used esp. in tennis, badminton, and squash.
+}
+radical
+{
+Noun:A person who advocates thorough or complete political or social reform; a member of a political party or part of a party pusuing such aims. 
+Adjective:(esp. of change or action)Relating to or affecting the fundamental nature of something;far-reaching or thorough.
+}
+rage
+{
+Noun:Violent, uncontrollable anger. The violent action of a natural agency. 
+Verb:Feel or express violent uncontrollable anger.
+}
 wage salary.
-
+unfamiliar
+{
+prodigious
+proficient
+prohibitive
+prolific
+pronounced
+proponent
+prose
+prospect
+prudent
+puddle
+pulp
+punctual
+purple
+quaint
+quarry
+quilt
+racket
+}
--- 26.c
+++ 26.c
@@ -1,14 +1,18 @@
 rally
 {
-Verb:Subject(someone)to good-humored ridicule;tease. Verb:(of troops)Come together again in order to continue fighting after a defeat or dispersion. Noun:A mass meeting of people making a political protest or showing support for a cause.
+Verb:Subject(someone)to good-humored ridicule;tease. 
+Verb:(of troops)Come together again in order to continue fighting after a defeat or dispersion. 
+Noun:A mass meeting of people making a political protest or showing support for a cause.
 }
 ranch
 {
-Verb:Runa ranch. Breed(animals)on a ranch. Use(land)as a ranch. Noun:A large farm, esp.in the western US and Canada, where cattle or other animals are bred and raised.
+Verb:Runa ranch. Breed(animals)on a ranch. Use(land)as a ranch. 
+Noun:A large farm, esp.in the western US and Canada, where cattle or other animals are bred and raised.
 }
 rash
 {
-Adjective:Displaying or proceeding from a lack of careful consideration of the possible consequences of an action. a rash decision. Noun:An area of reddening of a person's skin, sometimes raised apots, appearing esp. as a result of allergy or illness.
+Adjective:Displaying or proceeding from a lack of careful consideration of the possible consequences of an action. a rash decision. 
+Noun:An area of reddening of a person's skin, sometimes raised apots, appearing esp. as a result of allergy or illness.
 }
 ratify
 {
@@ -16,15 +20,18 @@ Verb:Sign or give formal consent to(a treaty, contract, or agreement), making it
 }
 ration
 {
-Noun:A fixed amount of a commodity officially allowed to each person during a time of shortage, as in wartime. Verb:Allow each person to have only a fixed amount of(a particular commodity).
+Noun:A fixed amount of a commodity officially allowed to each person during a time of shortage, as in wartime. 
+Verb:Allow each person to have only a fixed amount of(a particular commodity).
 }
 rational
 {
-Noun:A rational number. Adjective:Based on or in accordance with reason or logic. (of a person)Able to think clearly, sensibly, and logically.
+Noun:A rational number. 
+Adjective:Based on or in accordance with reason or logic. (of a person)Able to think clearly, sensibly, and logically.
 }
 rattle
 {
-Verb:Make a rapid succession of short, sharp knocking sounds, typically as a result of being shaken and striking repeatedly against a hard surface or object. Noun:A rapid succession of short, sharp, hard sounds.
+Verb:Make a rapid succession of short, sharp knocking sounds, typically as a result of being shaken and striking repeatedly against a hard surface or object. 
+Noun:A rapid succession of short, sharp, hard sounds.
 }
 raw
 {
@@ -40,11 +47,14 @@ Noun:A kindom. A field or domain of activity or interest. A primary biogeographi
 }
 rear
 {
-Adjective:At the back.the car's rear window. Verb:Bring up and care for(a child)until they are fully grown, esp. in a particular manner or place. Noun:The back part of something, esp. a building or vehicle.
+Adjective:At the back.the car's rear window. 
+Verb:Bring up and care for(a child)until they are fully grown, esp. in a particular manner or place. 
+Noun:The back part of something, esp. a building or vehicle.
 }
 rebel
 {
-Noun:A person who rises in opposition or armed resistance against an established government or ruler. A person who resists authority, control, or convention. Verb:Rise in opposition or armed resistance to an established government or ruler.
+Noun:A person who rises in opposition or armed resistance against an established government or ruler. A person who resists authority, control, or convention. 
+Verb:Rise in opposition or armed resistance to an established government or ruler.
 }
 receptacle
 {
@@ -60,12 +70,14 @@ Adjective:(of a person or their actions)Without thinking or caring about the con
 }
 recreation
 {
-Noun:The action or process of creating something again. Noun:Activity done for enjoyment when one is not working.
+Noun:The action or process of creating something again. 
+Noun:Activity done for enjoyment when one is not working.
 }
 reddish
 reef
 {
-Noun:A ridge of jagged rock, coral, or sand just above or below the surface of the sea. A metalliferous mineral deposit, esp.one that is bedded and contains gold. Verb:Take in one or more reefs of(a sail).
+Noun:A ridge of jagged rock, coral, or sand just above or below the surface of the sea. A metalliferous mineral deposit, esp.one that is bedded and contains gold. 
+Verb:Take in one or more reefs of(a sail).
 }
 refine
 {
@@ -85,7 +97,8 @@ Verb:Renovate and redocorate(something, esp. a building).
 }
 regenerate
 {
-Adjective:Reformed or reborn, esp. in a spiritual or moral sense. Verb:(of a living organism)Regrow(new tissue)to replace lost or injured tissue.
+Adjective:Reformed or reborn, esp. in a spiritual or moral sense. 
+Verb:(of a living organism)Regrow(new tissue)to replace lost or injured tissue.
 }
 regulate
 {
@@ -105,7 +118,8 @@ Verb:Strengthen or support, esp. with additional personnel or material. Strength
 }
 relay
 {
-Verb:Receive and pass on(information or a message). Noun:A group of people or animals engaged in a task or activity for a fixed period of time and the replaced by a similar group.
+Verb:Receive and pass on(information or a message). 
+Noun:A group of people or animals engaged in a task or activity for a fixed period of time and the replaced by a similar group.
 }
 relief
 {
@@ -125,7 +139,8 @@ Adjective:Worthy of attention; striking.
 }
 remnant
 {
-Adjective:Remaining. Noun:A small remaining quantity of something.
+Adjective:Remaining. 
+Noun:A small remaining quantity of something.
 }
 removal
 {
@@ -135,4 +150,30 @@ revival
 renaissance
 {
 Noun:The revival of art and literature under the influence of classical models in the 14th-16th centuries.
+}
+unfamiliar
+{
+rally
+ranch
+rash
+ratify
+ration
+rattle
+raw
+realm
+rear
+receptacle
+reckless
+refine
+refurbish
+rehabilitate
+rehearse
+reinforce
+relay
+relief
+relieve
+reluctant
+remarkable
+remnant
+renaissance
 }
