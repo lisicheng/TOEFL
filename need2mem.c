9824d7051a5af41d4ce15ee215dd6d7f19148c9f
Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>
2010-12-17 12:02:10 +0800

    add .c for need2mem
    Signed-off-by: Sicheng LI in lisicheng:lisicheng <lisicheng2008@gmail.com>

---
+++ need2mem.c
@@ -0,0 +1,260 @@
+dubious
+{
+Adjective:Hesitating or doubting.
+}
+ductile
+{
+Adjective:(of a metal)Able to be drawn out into a thin wire.
+}
+eliminate
+
+eminent
+endure
+{
+endure such pain.
+}
+enormous
+{
+gigantic.
+Adjective:Very large in size, quantity, or extent.
+}
+entail
+{
+Noun:A settlement of the inheritance of property over a number of generations so that it remains within a family or other group.
+Verb:Involve(something)as a necessary or inevitable part or consequence.
+}
+ephemeral
+{
+transient.
+Adjective:Lasting for a very short time.
+}
+episode
+epitomize
+{
+Verb:Be a perfect example of.
+    Give a summary of(a written work).
+}
+erect
+{
+Adjective:Rigidly upright or straight.
+Verb:Construct(a building, wall, or other upright structure).
+}
+ethnic
+{
+cultural origins.
+}
+evacuation
+{
+Noun:The action of evacuating a person or a place.
+A quantity of matter discharged from the bowels or another bodily organ.
+}
+glow
+{
+Noun:A steady radiance of light or heat.
+}
+
+herald
+{
+announce, declare. and offical messenger bringing news.
+}
+hierarchy
+hitherto
+homogeneous
+humid
+{
+moist, damp, wet.
+}
+hydrothermal
+hypothesize
+{
+suppose.
+}
+
+allay
+{
+Verb:Diminish or put at rest(fear, suspicion, or worry)
+    Relieve or alleviate(pain or hunger).
+}
+allegory
+allergy
+alleviate
+amnesia
+{
+Noun:A partial or total loss of memory.
+}
+anthropology
+antibiotic
+
+anticipate
+{
+look forward to.
+}
+apprentice
+arid
+{
+drought.
+Adjective:(of land or a climate)Having little or no rain; too dry or barren to support vegetation.
+}
+articulate
+{
+having joint segments.
+}
+
+divergent
+{
+Adjective:Tending to be different or develop in different directions.
+}
+temptation
+{
+Noun:A desire to do something, esp. something wrong or unwise.
+    A thing or course of action that attracts or tempts someone.
+}
+barefaced
+{
+Adjective:Shameless; undisguised.
+}
+berserk
+{
+Adjective:(of a person or animal)Out of control with anger or excitement; wild or frenzied.
+    (of a mechanical device or system)Operating in a wild or erratic way; out of control.
+}
+freak
+{
+Verb:React of behave in a wild and irrational way; typically because of the effects of extreme emotion, mental illness, or drugs.
+Noun:A very unusual and unexpected event or situation.
+}
+
+//Word List 1 to 5
+    adverse
+    acute
+    acquaint
+accomplished
+
+articulate
+    artisan
+archaeology
+apprentice
+    anticipate
+anatomy
+amino
+ammonia
+amnesia
+allegory
+
+biography
+blade
+bleach
+    benign
+    barter
+barn
+bald
+balcony
+    avert
+    augment
+auditorium
+audit
+attorney
+
+    cardinal
+    caribou
+    carnivore
+    cartilage
+    carve
+    cannibalism
+canopy
+cape
+    calibration
+    camouflage
+bulk
+burgeon
+burrow
+brittle
+brass
+boast
+bold
+
+coherent
+coax
+clumsy
+clump
+clipper
+    clamor
+chill
+chlorine
+cholesterol
+choreographer
+checked
+    chafe
+celestial
+cascara
+
+//Word List 6 to 10
+    confederacy
+comprise
+    concede
+    conceive
+    compliment
+    competence
+    commemorate
+combustible
+collide
+
+counselor
+counteract
+    counterpart
+coupon
+    coordinate
+convention
+    converge
+continuum
+    consumption
+contagious
+congenial
+    congestion
+consent
+content
+
+delicate
+deliberate
+definitive
+defect
+deceptive
+debris
+debut
+decent
+dairy
+dawn
+cuneiform
+crush
+crest
+crippling
+crow
+crawl
+cramped
+
+dilemma
+dictate
+devastate
+devise
+devour
+deteriorate
+detest
+desolate
+destined
+descent
+depict
+deplete
+deposit
+deprive
+depart
+delta
+
+elevate
+elaborate
+dwarf
+dramatic
+dormant
+disturbance
+dispose
+
+//Word List 11 to 15
