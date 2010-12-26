921bd85856646d345470f9dc8caad8b32ae2a486
Sicheng LI <lisicheng2008@gamil.com>
2010-12-26 23:19:58 +0800

    add words' explanations for 33.c
    Signed-off-by: Sicheng LI <lisicheng2008@gamil.com>

--- 33.c
+++ 33.c
@@ -1,55 +1,262 @@
 teem
+{
+Verb:Be full of or swarming with. every garden is teeming with wildlife.
+    (of water, esp. rain)Pour down; fall heavily.
+}
 tenant
+{
+Verb:Occupy(property)as a tenant.
+Noun:A person who occupies land or property rented from a landlord.
+    A person holding real property by private ownership.
+}
 tendon
+{
+Noun:A flexible inelastic cord of strong fibrous collagen tissue attaching a muscle to a bone.
+    The hamstring of a quadruped.
+}
 tenement
+{
+Noun:A room or a set of rooms forming a separate residence within a house or block of apartments.
+}
 tension
+{
+Noun:The state of being stretched tight.
+    The state of having the muscles stretched tight, esp. as causing strain or discomfort.
+Verb:Apply a force to(something)that tends to stetch it.
+}
 tentacle
+{
+Noun:A slender flexible limb or appendage in an animal, esp. around the mouth of an invertebrate, used for grasping, moving about, or bearing sense organs.
+}
 tenuous
+{
+Adjective:Very weak or slight.
+    Very slender of fine; insubstantial.
+}
 terminal
+{
+Noun:An end or extremity of something, in particular.
+    The end of a railroad or other transport route, or a station at such a point.
+Adjective:Of, forming, or situated at the end or extremity of something.
+}
 terminology
+{
+Noun:The body of terms used with a particular technical application in a subject of study, theory, profession, etc.
+}
 termite
+{
+Noun:A small, pale soft-bodied insect that lives in large colonies with several different castes, typically with a mound of cemented earth. Many kinds feed on wood and can be highly destructive to trees and timber.
+}
 terrain
+{
+Noun:A stretch of land, esp. with regard to its physical features.
+}
 terrestrial
+{
+Adjective:Of, on, or relating to the earth.
+    Denoting television broadcast using equipment situated on the ground rather than by satellite.
+Noun:An inhabitant of the earth.
+}
 territory
+{
+Noun:An area of land under the jurisdiction of a ruler or state.
+    An area defended by a team or player in a game or sport.
+}
 testimony
+{
+Noun:A formal written or spoken statement, esp. one given in a court of law.
+    A solemn protest or declaration.
+}
 textile
+{
+Noun:A type of cloth or woven fabric.
+    The branch of industry involved in the manufacture of cloth.
+Adjective:Of or relating to fabric weaving.
+}
 texture
+{
+Noun:The feel, appearance, or consistency of a surface or a substance.
+Verb:Give(a surface, esp. of a fabric or wall covering)a rough or raised texture.
+}
 thaw
+{
+Noun:A period of warmer weather that thaws ice and snow.
+Verb:(of ice, snow, or another frozen substance, such as food)Become liquid or soft as a result of warming.
+}
 therapy
+{
+Noun:Treatment intended to relieve or heal a disorder.
+    The treatment of mental or psychological disorders by psycological means.
+}
 thorn
+{
+Noun:A stiff, sharp-pointed, straight or curved woody projection on the stem or other part of a plant.
+}
 thorough
+{
+Adjective:Complete with regard to every detail; not superficial or partial.
+}
 thrifty
+{
+Adjective:(of a person or their behavior)Using money and other resources carefully and not wastefully.
+}
 thrive
+{
+Verb:(of a child, animal, or plant)Grow or develop well or vigorously.
+    Prosper; flourish.
+}
 thwart
+{
+Verb:Prevent(someone)from accomplishing something.
+    Oppose(a plan, attempt, or ambition)successfully.
+Noun:A structural crosspiece sometimes forming a seat for a rower in a boat.
+Adverb:From one side to another side of; across.
+}
 tile
+{
+Noun:A thin rectangular slab of baked clay, concrete, or other material, used in overlapping rows for covering roofs.
+Verb:Cover(something)with files.
+}
 tolerant
+{
+Adjective:Showing willingness to allow the existence of opinions or behavior that one does not necessarily agree with.
+    (of a plant, animal, or machine)Able to endure(specified conditions or treatment).
+}
 topography
+{
+Noun:The arrangement of the natural and artificial physical features of an area.
+    A detailed description or representation on a map of such features.
+}
 topsoil
 tornado
 torrent
+{
+Noun:A strong and fast-moving stream of water or other liquid.
+    A sudden, violent, and copious outpouring of(something, typically words or feelings).
+}
 tournament
+{
+Noun:(in a sport game)A series of contests between a number of competitors, who compete for and overall prize.
+}
 tow
+{
+Verb:(of a motor vehicle or boat)Pull(another vehicle or boat)along with a rope, chain, or tow bar.
+Noun:An act of towing a vehicle or boat.
+}
 towering
+{
+Adjective:Extremely tall, esp. in comparison with the surroundings.
+    Of exceptional importance or influence.
+}
 toxic
+{
+Adjective:Poisonous.
+    Relating to or caused by poison.
+    Very bad, unpleasant, or harmful.
+Noun:Poisonous substance.
+}
 trace
+{
+Verb:Find or discover by investigation.
+Noun:Each of the two side straps, chains, or ropes by which a horse is attached to a vehicle that it is pulling.
+Noun:A mark, object, or other indication of the existence or passing of something.
+}
 tract
+{
+Noun:A short treatise in pamphlet form, typically on a religious subject.
+Noun:An area of indefinite extent, typically a large one.
+}
 tragedy
+{
+Noun:An event causing great suffering, destruction, and distress, such as a serious accident, crime, or natural catastrophe.
+}
 tragic
+{
+Adjective:Causing or characterized by extreme distress or sorrow.
+    Suffering extreme distress or sorrow.
+}
 trait
+{
+Noun:A distinguishing quality or characteristic, typically one belonging to a person.
+}
 transcend
+{
+Verb:Be or go beyond the range or limits of(something abstract, typically a conceptual field or division)
+}
 transit
+{
+Verb:Pass across or through(an area).
+Noun:The carrying of people, goods, or materials from one place to another.
+}
 transition
+{
+Verb:Undergo or cause to undergo a process or period of transition.
+Noun:The process or a period of changing from one state or condition to another.
+}
 translucent
+{
+Adjective:(of a substance)Allowing light, but not detailed images, to pass through; semitransparent.
+}
 transmit
+{
+Verb:Cause(something)to pass on from on place or person to another.
+    Broadcast or send out(an electrical signal or a radio or television program).
+}
 transplant
+{
+Verb:Move or tranfer(something)to another place or situation, typically with some effort or upheaval.
+Noun:An operation in which an organ or tissue is transplanted.
+}
 trash
 tread
+{
+Noun:A manner or the sound of someone walking.
+    The stop surface of a step or stair.
+Verb:Walk in a specified way.
+}
 tremendous
+{
+Adjective:Very great in amount, scale, or intensity.
+    Extremely good or impressive; excellent.
+}
 trench
+{
+Verb:Dig a trench or trenches in(the ground).
+Noun:A long, narrow ditch.
+    Such a ditch dug by troops to provide a place of shelter from enemy fire.
+}
 tribal
+{
+Adjective:Of or characteristic of a tribe or tribes.
+Noun:Members of tribal communities, esp. in the Indian subcontinent.
+}
 tributary
+{
+Noun:A river or stream flowing into a larger river or lake.
+    A person or state that pays tribute to another state or ruler.
+}
 trigger
+{
+Noun:A small device that releases a spring or catch and so sets off a mechanism, esp. in order to fire a gun.
+Verb:Cause(an event or situation)to happen or exist.
+}
 trivial
+{
+Adjective:Of little value or importance.
+    (of a person)Concerned only with trifling or unimportant things.
+}
 troupe
+{
+Noun:A group of dancers, actors, or other entertainers who tour to different venues.
+}
 tube
+{
+Verb:Provide with a tube or tubes.
+Noun:A long, hollow cylinder of metal, plastic, glass, etc., for holding or transporting something, chiefly liquids or gases.
+}
 tug
+{
+Noun:A hard or sudden pull.
+Verb:Pull(something)hard or suddenly.
+}
+
