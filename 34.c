29eafd19f5ceaf110bd23b8d7d9a2e0b73afeb7b
LiSicheng <u@LENOVO-4EA874F2.(none)>
2011-01-02 13:50:18 +0800

    modifed 34

--- 34.c
+++ 34.c
@@ -1,7 +1,22 @@
 tundra
+{
+Noun:A vast, flat, treeless Arctic region of Europe, Asia, and North American in which the subsoil is permanently frozen.
+}
 turbulent
+{
+Adjective:Characterized by conflict, disorder, or confusion; not controlled or calm.
+    (of air or water)Moving unsteadily or violently.
+}
 turnpike
+{
+Noun:An expressway, esp. one on which a toll is charged.
+    A road on which a toll was collected at such a gate.
+}
 tutor
+{
+Verb:Act as a tutor to(a single student or a very small group).
+Noun:A private teacher, typically who teaches a single student or a very small group.
+}
 twig
 twine
 twinkling
