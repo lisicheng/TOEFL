ebac8ede8b596b3bc753933da539ba313d66ee44
LiSicheng <u@LENOVO-4EA874F2.(none)>
2011-01-02 13:29:50 +0800

    add dailywork.c

---
+++ dailywork.c
@@ -0,0 +1,50 @@
+mnemonics
+{
+Noun:The study and development of systems for improving and assisting the memory.
+}
+delimit
+{
+Verb:Determine the limits or boundaries of.
+}
+parlance
+{
+Noun:A particular way of speaking or using words.
+}
+backslash
+analogous
+{
+Adjective:Comparable in certain respects, typically in a way that makes clearer the nature of the things compared.
+}
+explicit
+{
+Noun:The closing words of a text, manuscript, early printed book, or chanted liturgical text.
+Adjective:Stated clearly and in detail, leaving no room for confusion or doubt.
+}
+fiscal
+{
+Adjective:Of or relating to government revenue, esp. taxes.
+					Of or relating to financial matters.
+}
+sluggish
+{
+Adjective:Slow moving or inactive.
+}
+triplets
+{
+Noun:One of three children or animals born at the same time.
+}
+gamble
+{
+Verb:Bet(a sum of money)in such a way.
+}
+cascaded
+nomenclature
+{
+Noun:The devising or choosing of names for things, esp. in a science or other decipline.
+}
+parentheses
+bracket
+copious
+{
+Adjective:Abundant in supply or quantity.
+}
